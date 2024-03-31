c1ae94c230eae25afbc84827ba69e63527976f68
biaobro <biaobro@gmail.com>
2024-03-31 22:31:02 +0800

    FS001 add click process for menu and sidebar item

---
+++ FS001-Dashboard Design #01/js/script.js
@@ -0,0 +1,24 @@
+const menuOpen = document.getElementById('menu-open');
+const menuClose = document.getElementById('menu-close');
+const sideBar = document.querySelector('.container .left-section');
+const sideBarItems = document.querySelectorAll('.container .left-section .sidebar .item');
+
+menuOpen.addEventListener('click', ()=>{
+    sideBar.style.top = '0';
+})
+
+menuClose.addEventListener('click', ()=>{
+    sideBar.style.top = '-60vh';
+})
+
+let activeItem = sideBarItems[0];
+
+sideBarItems.forEach(element => {
+    element.addEventListener('click', ()=>{
+        if (activeItem){
+            activeItem.removeAttribute('id')
+        }
+        element.setAttribute('id', 'active');
+        activeItem = element;
+    })
+})
