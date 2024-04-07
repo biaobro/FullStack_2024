88d5e3b8fc6b1dbad907c53065fff3dfe8a50cce
biaobro <biaobro@gmail.com>
2024-04-07 22:09:22 +0800

    FS002 add hero section

--- FS002-Business Agency/js/script.js
+++ FS002-Business Agency/js/script.js
@@ -29,3 +29,49 @@ window.addEventListener("scroll", function(){
         header.classList.remove("active")
     }
 })
+
+// slider
+const sliders = document.querySelectorAll("[data-slider]")
+const initSlider = function(currentSlider){
+    const sliderContainer = currentSlider.querySelector("[data-slider-container]")
+    const sliderPrevBtn = currentSlider.querySelector("[data-slider-prev]")
+    const sliderNextBtn = currentSlider.querySelector("[data-slider-next]")
+
+    let currentSlidePos = 0;
+    const moveSliderItem = function (){
+        sliderContainer.style.transform = `translateX(-${sliderContainer.children[currentSlidePos].offsetLeft}px)`
+    }
+
+    // next slide
+    const slideNext = function(){
+        const slideEnd = currentSlidePos >= sliderContainer.childElementCount - 1;
+        if(slideEnd){
+            currentSlidePos = 0;
+        }else {
+            currentSlidePos++
+        }
+        moveSliderItem();
+    }
+    sliderNextBtn.addEventListener("click", slideNext)
+
+    // previous slide
+    const slidePrev = function(){
+        if(currentSlidePos <= 0){
+            currentSlidePos = sliderContainer.childElementCount - 1;
+        }else {
+            currentSlidePos--
+        }
+        moveSliderItem();
+    }
+    sliderPrevBtn.addEventListener("click", slidePrev)
+
+    const dontHaveExtraItem = sliderContainer.childElementCount <= 1;
+    if(dontHaveExtraItem){
+        sliderNextBtn.style.display = "none";
+        sliderPrevBtn.style.display = "none"
+    }
+}
+
+for (let i=0,len=sliders.length; i<len; i++){
+    initSlider(sliders[i])
+}
