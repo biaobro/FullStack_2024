7675441d019d0ce2edb45f70dffc4cd5e3ae307e
biaobro <biaobro@gmail.com>
2024-04-20 12:42:04 +0800

    FS003 add Responsive Design Section

--- FS003-Wedding Agency Website/js/script.js
+++ FS003-Wedding Agency Website/js/script.js
@@ -70,3 +70,12 @@ const initNavigation = () => {
     })
     sectionNavigator('about')
 }
+
+
+// toggle menu
+const toggleMenu = () => {
+    const menu = document.querySelector('.menu')
+    const navMobile = document.querySelector('.nav-mobile')
+    menu.classList.toggle('active')
+    navMobile.classList.toggle('active')
+}
