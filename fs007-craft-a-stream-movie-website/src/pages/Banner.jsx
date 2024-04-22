ef6250cac986e1d37f3530794b32e89a6bf62267
biaobro <biaobro@gmail.com>
2024-04-22 08:16:13 +0800

    FS007 add banner section

---
+++ fs007-craft-a-stream-movie-website/src/pages/Banner.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './header.css';
+
+function Banner(props) {
+    return (
+        <div className="banner">
+            Banner
+        </div>
+    );
+}
+
+export default Banner;
