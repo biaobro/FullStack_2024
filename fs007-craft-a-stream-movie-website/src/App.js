7bf56f4cedc6ea20a4e837ea997d39705d603d4c
biaobro <biaobro@gmail.com>
2024-04-27 22:24:05 +0800

    FS007 add BackToTopBtn

--- fs007-craft-a-stream-movie-website/src/App.js
+++ fs007-craft-a-stream-movie-website/src/App.js
@@ -9,17 +9,35 @@ import Header from "./pages/Header";
 import Banner from "./pages/Banner";
 import Main from "./pages/Main";
 import Footer from "./pages/Footer";
+import BackToTopBtn from "./components/BackToTopBtn";
+import {useEffect, useState} from "react";
 
 function App() {
-  return (
+    // the scroll position of window
+    const [scroll, setScroll] = useState(0)
+
+    // constantly listen to the scroll position
+    useEffect(() => {
+        window.addEventListener('scroll', () => {
+            setScroll(window.scrollY)
+        })
+        return () => {
+            window.removeEventListener('scroll', () => {
+                setScroll(window.scrollY)
+            })
+        }
+    }, [scroll])
+
+    return (
       <>
-          <Header/>
+          <Header scroll={scroll}/>
           <Banner/>
           <Main />
           {/* put Footer here doesn't work, move it inside of Main*/}
           {/*<Footer/>*/}
+          <BackToTopBtn scroll={scroll}/>
       </>
-  );
+    );
 }
 
 export default App;
