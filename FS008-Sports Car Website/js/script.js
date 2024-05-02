783daba93cf9be2453b0ad74c6af48fff7e0f88e
biaobro <biaobro@gmail.com>
2024-05-02 19:11:34 +0800

    FS008 change background and trailer videos

--- FS008-Sports Car Website/js/script.js
+++ FS008-Sports Car Website/js/script.js
@@ -5,3 +5,31 @@ function toggleMenu(){
     menu.classList.toggle('active')
     nav.classList.toggle('active')
 }
+
+// Change the background video when click on the gallery images
+function changeVideo(name){
+    const bgVideoList = document.querySelectorAll('.bg-video')
+    const trailers = document.querySelectorAll('.trailer')
+    const models = document.querySelectorAll('.model')
+
+    bgVideoList.forEach(video =>{
+        video.classList.remove('active')
+        if(video.classList.contains(name)){
+            video.classList.add('active')
+        }
+    })
+
+    models.forEach(model =>{
+        model.classList.remove('active')
+        if(model.classList.contains(name)){
+            model.classList.add('active')
+        }
+    })
+
+    trailers.forEach(video =>{
+        video.classList.remove('active')
+        if(video.classList.contains(name)){
+            video.classList.add('active')
+        }
+    })
+}
