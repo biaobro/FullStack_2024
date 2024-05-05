4d48ae29626d1339a17c05062e2ddbaddc7a99cc
biaobro <biaobro@gmail.com>
2024-05-06 01:21:21 +0800

    FS010 View Information of specific User

--- FS010-Full Stack Web App/fullstack-front/src/layout/Navbar.js
+++ FS010-Full Stack Web App/fullstack-front/src/layout/Navbar.js
@@ -6,7 +6,7 @@ function Navbar(props) {
         <div>
             <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
                 <div className="container-fluid">
-                    <a className="navbar-brand" href="#">Full Stack Application</a>
+                    <Link className="navbar-brand" to="/">Full Stack Application</Link>
                     <button className="navbar-toggler" type="button" data-bs-toggle="collapse"
                             data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false"
                             aria-label="Toggle navigation">
