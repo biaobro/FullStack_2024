16bffab1d2664e462903ed48d607e8302a49951d
biaobro <biaobro@gmail.com>
2024-05-05 22:24:06 +0800

    FS010 Create Navbar using Bootstrap and React

---
+++ FS010-Full Stack Web App/fullstack-front/src/layout/Navbar.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+function Navbar(props) {
+    return (
+        <div>
+            <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
+                <div className="container-fluid">
+                    <a className="navbar-brand" href="#">Full Stack Application</a>
+                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse"
+                            data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false"
+                            aria-label="Toggle navigation">
+                        <span className="navbar-toggler-icon"></span>
+                    </button>
+
+                    <button className="btn btn-outline-light">Add user</button>
+                </div>
+            </nav>
+        </div>
+    );
+}
+
+export default Navbar;
