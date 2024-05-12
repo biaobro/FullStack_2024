f4ab8caee9c1ee18e6dbdb24e7f0c7cdbab7f46a
biaobro <biaobro@gmail.com>
2024-05-12 19:26:08 +0800

    FS011 frontend Create Home and Hero Component

--- FS011-Full Stack Movies/movie-client/src/App.js
+++ FS011-Full Stack Movies/movie-client/src/App.js
@@ -1,10 +1,13 @@
 import './App.css';
 import api from './api/axiosConfig.js'
 import {useState, useEffect} from "react";
+import {Route, Routes} from "react-router-dom";
+import Layout from "./components/Layout";
+import Home from "./components/Home";
 
 
 function App() {
-  const [movies, setMovies] = useState();
+  const [movies, setMovies] = useState([]);
 
   const getMovies = async () =>{
     try
@@ -25,7 +28,11 @@ function App() {
 
   return (
     <div className="App">
-
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route path="/" element={<Home movies={movies}/>}></Route>
+        </Route>
+      </Routes>
     </div>
   );
 }
