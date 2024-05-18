0368e38cf14ed3636881c24e6b11a12b919f40bf
biaobro <biaobro@gmail.com>
2024-05-18 23:37:22 +0800

    FS011 frontend Course Wrap Up

--- FS011-Full Stack Movies/movie-client/src/App.js
+++ FS011-Full Stack Movies/movie-client/src/App.js
@@ -7,6 +7,7 @@ import Home from "./components/Home";
 import Header from "./components/Header";
 import Trailer from "./components/Trailer";
 import Reviews from "./components/Reviews";
+import NotFound from "./components/NotFound";
 
 
 function App() {
@@ -53,6 +54,7 @@ function App() {
           <Route path="/" element={<Home movies={movies}/>}></Route>
           <Route path="/Trailer/:ytTrailerId" element={<Trailer />}></Route>
           <Route path="/Reviews/:movieId" element={<Reviews getMovieData={getMovieData} movie={movie} reviews={reviews} setReviews={setReviews}/>}></Route>
+          <Route path="*" element={<NotFound />} />
         </Route>
       </Routes>
     </div>
---
+++ FS011-Full Stack Movies/movie-client/src/components/NotFound.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const NotFound = () => {
+    return (
+        <div>
+            This page could not been found.
+        </div>
+    );
+};
+
+export default NotFound;
