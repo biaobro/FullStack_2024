9a7577fdc2fb906807f943fb5663b73df7c2c6f8
biaobro <biaobro@gmail.com>
2024-05-26 17:24:28 +0800

    FS014 frontend add route for every button

--- FS014-Full Stack E-Commerce Website/frontend/src/App.js
+++ FS014-Full Stack E-Commerce Website/frontend/src/App.js
@@ -1,10 +1,29 @@
 import './App.css';
 import Navbar from "./Components/Navbar/Navbar";
+import {BrowserRouter, Route, Routes} from "react-router-dom";
+import Shop from "./Pages/Shop";
+import ShopCategory from "./Pages/ShopCategory";
+import Product from "./Pages/Product";
+import Cart from "./Pages/Cart";
+import LoginSignup from "./Pages/LoginSignup";
 
 function App() {
   return (
     <div>
-      <Navbar />
+        <BrowserRouter>
+            <Navbar />
+            <Routes>
+                <Route path='/' element={<Shop/>}></Route>
+                <Route path='/men' element={<ShopCategory category="men"/>}></Route>
+                <Route path='/women' element={<ShopCategory category="women"/>}></Route>
+                <Route path='/kids' element={<ShopCategory category="kids"/>}></Route>
+                <Route path="/product" element={<Product />}>
+                    <Route path=':productId' element={<Product/>}/>
+                </Route>
+                <Route path='/cart' element={<Cart/>}></Route>
+                <Route path='/login' element={<LoginSignup/>}></Route>
+            </Routes>
+        </BrowserRouter>
     </div>
   );
 }
