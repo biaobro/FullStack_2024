fa03ae15035d5d9fb47b3c193c895ca0ad0020b3
biaobro <biaobro@gmail.com>
2024-06-09 00:10:05 +0800

    FS014 backend add product add API

--- FS014-Full Stack E-Commerce Website/backend/index.js
+++ FS014-Full Stack E-Commerce Website/backend/index.js
@@ -12,7 +12,7 @@ app.use(express.json());
 app.use(cors());
 
 // Database connection with MongoDB
-mongoose.connect("mongodb+srv://biaobro:Wazg2020@cluster0.hphndaf.mongodb.net")
+mongoose.connect("mongodb+srv://biaobro:Wazg2020@cluster0.hphndaf.mongodb.net/fs014")
 
 // API Creation
 app.get("/",(req, res)=>{
@@ -36,6 +36,75 @@ app.post("/upload", upload.single('product'),(req,res)=>{
     res.json({
         success:1,
         image_url:`http://localhost:${port}/images/${req.file.filename}`
+    })
+})
+
+// Schema for Creating Products
+const Product = mongoose.model("Product",{
+    id:{
+        type:Number,
+        required:true,
+    },
+    name:{
+        type:String,
+        required:true,
+    },
+    image:{
+        type:String,
+        required:true,
+    },
+    category:{
+        type:String,
+        required:true,
+    },
+    new_price:{
+        type:Number,
+        required:true,
+    },
+    old_price:{
+        type:Number,
+        required:true,
+    },
+    date:{
+        type:Date,
+        default:Date.now,
+    },
+    available:{
+        type:Boolean,
+        default:true,
+    },
+})
+
+app.post('/addproduct',async (req, res)=>{
+    let products = await Product.find({
+
+    })
+
+    let id;
+
+    if(products.length > 0){
+        console.log(`There already \`${products.length} products!\``)
+        let last_product_array = products.slice(-1);
+        let last_product = last_product_array[0]
+        id = last_product.id+1;
+    }else{
+        id = 1
+    }
+
+    const product = new Product({
+        id:id,
+        name:req.body.name,
+        image:req.body.image,
+        category:req.body.category,
+        new_price:req.body.new_price,
+        old_price:req.body.old_price,
+    })
+    console.log(product);
+    await product.save();
+    console.log("Saved!");
+    res.json({
+        success:true,
+        name:req.body.name,
     })
 })
 
