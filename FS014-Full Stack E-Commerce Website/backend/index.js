26f735220a9dc3aa2c32cb4f74a8c408e21b6dc6
biaobro <biaobro@gmail.com>
2024-06-10 23:49:09 +0800

    FS014 cart add product in backend and frontend

--- FS014-Full Stack E-Commerce Website/backend/index.js
+++ FS014-Full Stack E-Commerce Website/backend/index.js
@@ -187,7 +187,7 @@ app.post('/login', async (req,res)=>{
                     id:user.id
                 }
             }
-            const token = jwt.sign(data, 'secret-ecom')
+            const token = jwt.sign(data, 'secret_ecom')
             res.json({success:true, token})
         }
         else{
@@ -213,6 +213,33 @@ app.get("/popularinwomen", async (req,res)=>{
     let popularInWomen = products.slice(0,4)
     console.log("Popular In Women Fetched!")
     res.send(popularInWomen)
+})
+
+// Creating middleware to fetch user
+const fetchUser = async (req,res,next)=>{
+    const token = req.header('auth-token')
+    if(!token){
+        res.status(401).send({errors:"Please authenticate using valid token"})
+    }
+    else{
+        try{
+            const data = jwt.verify(token,'secret_ecom')
+            req.user = data.user;
+            next();
+        }catch(error){
+            res.status(401).send({errors:"Please authenticated using a valid token"})
+        }
+    }
+}
+
+
+// Creating endpoint for popular women data
+app.post('/addtocart', fetchUser, async (req,res)=>{
+    console.log(req.body,req.user)
+    let userData = await Users.findOne({_id:req.user.id})
+    userData.cartData[req.body.itemId] += 1;
+    await Users.findOneAndUpdate({_id:req.user.id},{cartData:userData.cartData})
+    res.send("Added")
 })
 
 
