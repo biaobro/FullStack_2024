635206a94bc06d8764b31fab1ceceb194f19ad37
biaobro <biaobro@gmail.com>
2024-06-15 17:43:37 +0800

    FS014 get products from cart in backend and frontend

--- FS014-Full Stack E-Commerce Website/backend/index.js
+++ FS014-Full Stack E-Commerce Website/backend/index.js
@@ -253,6 +253,13 @@ app.post('/removefromcart', fetchUser, async (req, res)=>{
     res.send("Removed")
 })
 
+// Creating endpoint to get cart data
+app.post('/getcart', fetchUser, async (req,res)=>{
+    console.log('Getcartdata')
+    let userData = await Users.findOne({_id:req.user.id})
+    res.json(userData.cartData)
+})
+
 app.listen(port, (error)=>{
     if(!error){
         console.log("Server Running on Port " + port)
