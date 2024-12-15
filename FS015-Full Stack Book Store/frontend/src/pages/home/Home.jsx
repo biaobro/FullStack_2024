634a157882ace4eecd7f5c3331d5988cc191faa2
biaobro <biaobro@gmail.com>
2024-12-15 22:54:06 +0800

    add Recommend Books section

--- FS015-Full Stack Book Store/frontend/src/pages/home/Home.jsx
+++ FS015-Full Stack Book Store/frontend/src/pages/home/Home.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import Banner from "./Banner.jsx";
 import TopSellers from "./TopSellers.jsx";
+import Recommended from "./Recommended.jsx";
 
 const Home = () => {
     return (
         <>
             <Banner />
             <TopSellers />
+            <Recommended />
         </>
     );
 };
