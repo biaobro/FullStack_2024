e7b40009b9953cf1a43ed703234c9d10e77dfce9
biaobro <biaobro@gmail.com>
2024-12-15 19:50:57 +0800

    add responsive navbar

--- FS015-Full Stack Book Store/frontend/src/App.jsx
+++ FS015-Full Stack Book Store/frontend/src/App.jsx
@@ -1,11 +1,12 @@
 import './App.css'
 import {Outlet} from "react-router-dom";
+import Navbar from "./components/Navbar.jsx";
 
 function App() {
 
   return (
     <>
-        <nav>Navbar</nav>
+        <Navbar></Navbar>
         <main className='min-h-screen max-w-screen-2xl mx-auto px-4 py-6 font-primary'>
             <Outlet/>
         </main>
