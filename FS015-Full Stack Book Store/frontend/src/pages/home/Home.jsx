0e9a8c2e594e831eacdceb6cd23abcf505ef6cda
biaobro <biaobro@gmail.com>
2024-12-15 22:44:27 +0800

    add Top Sellers Section

--- FS015-Full Stack Book Store/frontend/src/pages/home/Home.jsx
+++ FS015-Full Stack Book Store/frontend/src/pages/home/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import Banner from "./Banner.jsx";
+import TopSellers from "./TopSellers.jsx";
 
 const Home = () => {
     return (
         <>
             <Banner />
+            <TopSellers />
         </>
     );
 };
