6cf43e995e3cb7fef7b785d50fe653c27e320887
biaobro <biaobro@gmail.com>
2025-02-01 16:56:11 +0800

    FS015 setup mongoose and mongodb

--- FS015-Full Stack Book Store/backend/index.js
+++ FS015-Full Stack Book Store/backend/index.js
@@ -2,9 +2,23 @@ const express = require('express')
 const app = express()
 const port = process.env.PORT || 3000
 
-app.get('/', (req, res) => {
-    res.send('Book Server is running!')
-})
+const mongoose = require('mongoose');
+
+require('dotenv').config()
+
+async function main() {
+    await mongoose.connect(process.env.DB_URL_LOCAL)
+
+    // use local mongodb
+    // await mongoose.connect('mongodb://127.0.0.1:27017/test');
+    // use `await mongoose.connect('mongodb://user:password@127.0.0.1:27017/test');` if your database has auth enabled
+
+    app.get('/', (req, res) => {
+        res.send('Book Server is running haaaaa!')
+    })
+}
+
+main().then(() => console.log("Mongodb connect successfully!")).catch(err => console.log(err));
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
